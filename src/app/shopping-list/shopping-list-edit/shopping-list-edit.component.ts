ed435443195d4d9e753342ef84f42313cf829c17
Ashle3 <ashleeb3@byui.edu>
2024-02-10 20:53:51 -0600

    finished add ingredients

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from '../../shared/ingredients.model';
+import { ShoppingListService } from '../shoppint-list.service';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -9,12 +10,13 @@ import { Ingredient } from '../../shared/ingredients.model';
 export class ShoppingListEditComponent {
   @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
   @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
   
+  constructor(private shoppingListService: ShoppingListService) {}
+
   onAddItem() {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+    this.shoppingListService.addIngredient(newIngredient);
   }
 }
