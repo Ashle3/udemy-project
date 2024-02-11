29f440a46d2df2ba28d2226a4d8139ccd567666c
Ashle3 <ashleeb3@byui.edu>
2024-02-10 21:19:35 -0600

    finished w5 example

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,26 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredients.model";
+import { ShoppingListService } from "../shopping-list/shoppint-list.service";
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('Test', 'This is a test', 'https://mylifeandkids.com/wp-content/uploads/2013/12/Simple-Appetizer-Recipes.png')
+        new Recipe('Test', 'This is a test', 'https://mylifeandkids.com/wp-content/uploads/2013/12/Simple-Appetizer-Recipes.png',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('Bread', 2)
+        ])
     ];
+
+    constructor(private shoppingListService: ShoppingListService){}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.shoppingListService.addIngredients(ingredients);
     }
 }
